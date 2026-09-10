e9dfea6a234a9abae70ee8eb64b05d76e50d9f61
Engineering @ Channel <engineering@channel.surf>
2026-09-09 21:20:07 -0700

    Use pinned Forge mana shard names in X matching tests

--- forge-gui/src/test/java/forge/player/HumanManaXSmoke.java
+++ forge-gui/src/test/java/forge/player/HumanManaXSmoke.java
@@ -14,12 +14,12 @@ public final class HumanManaXSmoke {
         var b = new HumanManaX.Token(ManaAtom.BLACK, 0);
         var paid = new HumanManaX.Token(ManaAtom.WHITE | ManaAtom.BLACK, 1);
         check(0, List.of(), List.of());
-        check(0, List.of(ManaCostShard.W), List.of(w, paid));
-        check(1, List.of(ManaCostShard.W, ManaCostShard.W), List.of(w, b, paid));
-        check(1, List.of(ManaCostShard.W, ManaCostShard.B), List.of(paid, w));
-        check(1_000_000, List.of(ManaCostShard.W, ManaCostShard.W), List.of(w, b));
+        check(0, List.of(ManaCostShard.WHITE), List.of(w, paid));
+        check(1, List.of(ManaCostShard.WHITE, ManaCostShard.WHITE), List.of(w, b, paid));
+        check(1, List.of(ManaCostShard.WHITE, ManaCostShard.BLACK), List.of(paid, w));
+        check(1_000_000, List.of(ManaCostShard.WHITE, ManaCostShard.WHITE), List.of(w, b));
         check(1_000_000, List.of(ManaCostShard.GENERIC, ManaCostShard.GENERIC), List.of(paid));
-        check(0, List.of(ManaCostShard.GENERIC, ManaCostShard.W), List.of(paid, b, w));
+        check(0, List.of(ManaCostShard.GENERIC, ManaCostShard.WHITE), List.of(paid, b, w));
         System.out.println("PASS 7 exact X payment matching checks");
     }
 }
